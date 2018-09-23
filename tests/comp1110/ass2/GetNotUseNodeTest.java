f756ef837b6b42332c912505d9c0ed3089533c8b
Stella <ning.cai@anu.edu.au>
2018-09-24 06:51:53 +1000

    ..

--- tests/comp1110/ass2/GetNotUseNodeTest.java
+++ tests/comp1110/ass2/GetNotUseNodeTest.java
@@ -1,4 +1,17 @@
 package comp1110.ass2;
 
+import org.junit.Rule;
+import org.junit.rules.Timeout;
+
+import static org.junit.Assert.assertTrue;
+
 public class GetNotUseNodeTest {
+    @Rule
+    public Timeout globalTimeout = Timeout.millis(20000);
+
+    private void test(String in, boolean expected) {
+        boolean out = TwistGame.isPlacementWellFormed(in);
+        assertTrue("Input was '"+in+"', expected "+expected+" but got "+out, out == expected);
+    }
+
 }
