9cb6c9077a4d4600eef71339e44e292e02e4cbcb
Xiang Li <xiang.li2@anu.edu.au>
2018-10-17 03:08:46 +1100

    Redesign solution.

--- src/comp1110/ass2/Solution/DLX.java
+++ src/comp1110/ass2/Solution/DLX.java
@@ -1,4 +1,22 @@
 package comp1110.ass2.Solution;
 
 public class DLX {
+
+    class Node {
+        int size = 0;
+        int row, col; // col for debug
+        Node up, down, left, right, header;
+
+        Node(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        Node(Node header, int row, int col) {
+            this(row, col);
+            this.header = header;
+        }
+
+
+    }
 }
